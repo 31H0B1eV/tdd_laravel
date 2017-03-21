e0d3c328249272cf444ca5d40706621429600bf1
Artem Zinoviev <arzinoviev@gmail.com>
2017-03-21 13:32:17 +0300

    add more tests

--- tests/Unit/ConcertTest.php
+++ tests/Unit/ConcertTest.php
@@ -9,17 +9,36 @@ use Tests\BrowserKitTestCase;
 
 class ConcertTest extends BrowserKitTestCase
 {
-    use DatabaseMigrations;
+    // for now if using Model->make there is no need in migrations
+    // use DatabaseMigrations;
 
     /** @test */
     public function can_get_formatted_date()
     {
-        $concert = factory(Concert::class)->create([
+        $concert = factory(Concert::class)->make([ // use make instead off create for using in memory storage and prevent storing in database for speed
             'date' => Carbon::parse('2016-12-01 8:00pm'),
         ]);
 
-        $date = $concert->formatted_date;
+        $this->assertEquals('December 1, 2016', $concert->formatted_date);
+    }
 
-        $this->assertEquals('December 1, 2016', $date);
+    /** @test */
+    public function can_get_formatted_start_date()
+    {
+        $concert = factory(Concert::class)->make([
+            'date' => Carbon::parse('2016-12-01 17:00:00'),
+        ]);
+
+        $this->assertEquals('5:00pm', $concert->formatted_time);
+    }
+
+    /** @test */
+    public function can_get_ticket_price_in_dollars()
+    {
+        $concert = factory(Concert::class)->make([
+            'ticket_price' => 6750
+        ]);
+
+        $this->assertEquals('67.50', $concert->ticket_price_in_dollars);
     }
 }
